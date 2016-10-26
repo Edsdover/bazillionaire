a5d423207a502b846022fd70d26c27bb65628efc
Byron Dover <byrondover+github.com@gmail.com>
2016-10-26 08:33:36 -0700

    Add Company and Ship data models

--- README.md
+++ README.md
@@ -23,6 +23,45 @@ $ docker-compose up
 
 * <http://localhost:3000/explorer/swagger.json>
 
+### API Queries
+
+For more details on and examples of Loopback API query syntax, please reference
+<http://loopback.io/doc/en/lb2/Querying-data.html>.
+
+###### Find Ship by Company ID
+
+REST syntax.
+
+```http
+GET /api/Ships?filter[where][companyId]=1 HTTP/1.1
+```
+
+"Stringified" JSON syntax.
+
+Pass URL-encoded `{"where": {"companyId": 1}}` query object to `filter` GET
+parameter.
+
+```http
+GET /api/Ships?filter=%7B%22where%22%3A%20%7B%22companyId%22%3A%201%7D%7D HTTP/1.1
+```
+
+###### Find First 3 Games in Reverse Chronological Order
+
+REST syntax.
+
+```http
+GET /api/Players?filter[limit]=3&filter[order]=lastUpdated%20DESC HTTP/1.1
+```
+
+"Stringified" JSON syntax.
+
+Pass URL-encoded `{"limit": 3, "order": "lastUpdated DESC"}` query object to
+`filter` GET parameter.
+
+```http
+GET /api/Games?filter=%7B%22limit%22%3A%203%2C%20%22order%22%3A%20%22lastUpdated%20DESC%22%7D HTTP/1.1
+```
+
 ## Roadmap
 
 [Roadmap](ROADMAP.md)
--- server/boot/create-sample-models.js
+++ server/boot/create-sample-models.js
@@ -5,42 +5,14 @@ module.exports = function(app) {
   var mysqlDs = app.dataSources.mysqlDs;
 
   // create and seed models
-  var players = [];
-
-  async.parallel([
-    players: async.apply(createPlayers),
-  ], (err, results) => {
+  async.waterfall([
+    createPlayers,
+    createGames,
+    createCompanies,
+    createShips,
+  ], (err, result) => {
     if (err) throw err;
-    players = results.players;
-  });
-
-  var games = [];
-
-  async.parallel([
-    games: async.apply(createGames(players)),
-  ], (err, results) => {
-    if (err) throw err;
-    games = results.games;
-    //console.log('> Models created and seeded successfully.');
-  });
-
-  var companies = [];
-
-  async.parallel([
-    players: async.apply(createCompanies(games, players)),
-  ], (err, results) => {
-    if (err) throw err;
-    companies = results.companies;
-  });
-
-  //var ships = [];
-
-  async.parallel([
-    players: async.apply(createShips(companies)),
-  ], (err, results) => {
-    if (err) throw err;
-    //ships = results.ships;
-    console.log('> Models created and seeded successfully.');
+    console.log('> Model fixtures migrated and seeded successfully.');
   });
 
   // create players
@@ -71,13 +43,15 @@ module.exports = function(app) {
       var Game = app.models.Game;
       var DAY_IN_MILLISECONDS = 1000 * 60 * 60 * 24;
 
+      var games = [];
+
       Game.create([{
         name: "First Save",
         created: Date.now() - (DAY_IN_MILLISECONDS * 4),
         lastUpdated: Date.now() - (DAY_IN_MILLISECONDS * 4),
         playerId: players[0].id,
       }, {
-        name: "Damn You Hoff Meister!",
+        name: "Hoff Meister wins again!",
         created: Date.now() - (DAY_IN_MILLISECONDS * 3),
         lastUpdated: Date.now() - (DAY_IN_MILLISECONDS * 3),
         playerId: players[1].id,
@@ -87,13 +61,15 @@ module.exports = function(app) {
         lastUpdated: Date.now() - (DAY_IN_MILLISECONDS * 2),
         playerId: players[1].id,
       }, {
-        name: "Crew unhappy.",
+        name: "Crew happy.",
         created: Date.now() - DAY_IN_MILLISECONDS,
         lastUpdated: Date.now() - DAY_IN_MILLISECONDS,
         playerId: players[2].id,
-      }], cb);
+      }], (err, games) => {
+        cb(null, games, players);
+      });
 
-      // @TODO: Hacky. Implement mature database migrations.
+      // @TODO: Implement mature (or at least less hacky) database migrations.
       /*
       let sql = `
         ALTER TABLE Game
@@ -104,6 +80,77 @@ module.exports = function(app) {
 
       Game.dataSource.connector.execute(sql, () => {});
       */
+    });
+  }
+
+  // create companies
+  function createCompanies(games, players, cb) {
+    mysqlDs.automigrate('Company', function(err) {
+      if (err) return cb(err);
+
+      var Company = app.models.Company;
+
+      Company.create([{
+        name: "Dover Inc.",
+        gameId: games[0].id,
+        playerId: players[2].id,
+      }, {
+        name: "Lou Corp",
+        gameId: games[1].id,
+      }, {
+        name: "Gizzy Shipping",
+        gameId: games[1].id,
+      }, {
+        name: "Trading Corp. IV",
+        gameId: games[2].id,
+      }, {
+        name: "Vandergriff Ltd.",
+        gameId: games[3].id,
+      }, {
+        name: "Puffer Inc.",
+        gameId: games[3].id,
+      }, {
+        name: "Roke Transport",
+        gameId: games[3].id,
+      }, {
+        name: "Hoff Meister",
+        gameId: games[3].id,
+      }], cb);
+    });
+  }
+
+  // create ships
+  function createShips(companies, cb) {
+    mysqlDs.automigrate('Ship', function(err) {
+      if (err) return cb(err);
+
+      var Ship = app.models.Ship;
+
+      Ship.create([{
+        name: "The Stinger XII",
+        companyId: companies[0].id,
+      }, {
+        name: "The Fly Catcher",
+        companyId: companies[1].id,
+      }, {
+        name: "Le Rock",
+        companyId: companies[2].id,
+      }, {
+        name: "Whaler 2000",
+        companyId: companies[3].id,
+      }, {
+        name: "Retina",
+        companyId: companies[4].id,
+      }, {
+        name: "Cerebralis",
+        companyId: companies[5].id,
+      }, {
+        name: "The Globulizer",
+        companyId: companies[6].id,
+      }, {
+        name: "Locomotis",
+        companyId: companies[7].id,
+      }], cb);
     });
   }
 };
